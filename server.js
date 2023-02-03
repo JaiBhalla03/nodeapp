5c283159f55c93ca8e61ac7bd725a4025ea31ccb
jai <jai.bhalla2021@vitstudent.ac.in>
2023-02-03 19:36:48 +0530

    first commit

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ require('dotenv').config();
 const app = express();
 app.get('/', (req, res) => res.send('Deployed! 🚀'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 console.log(`Server is running on port ${process.env.PORT}`);
